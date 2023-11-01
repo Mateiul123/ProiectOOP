1d27c8ec3c5fb0002ab6b1eb06c5473236e17ac1
Mateiul123 <popmatei70@gmail.com>
2023-11-01 21:58:31 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,40 +1,21 @@
 #include <iostream>
 #include <vector>
 
-class Masina{
-private:
-    static int contorId;
-    const int id = 1;
-
 class masina{
-    static unsigned int contorID;
-    const unsigned int ID;
-    std::string Tip_masina;
+protected:
+    int Tip_masina;
     std::string culoare;
-    std::string motorizare;
-    unsigned int pret;
+    float motorizare;
+    float pret;
 public:
-    masina(const unsigned int id, const std::string &tipMasina, const std::string &culoare,
-           const std::string &motorizare, unsigned int pret) : ID(contorID++), Tip_masina(tipMasina), culoare(culoare),
+    std::vector<std::vector<float>> Customizari;
+
+    masina(const int &tipMasina, const std::string &culoare,
+           const float &motorizare, unsigned int pret) :  Tip_masina(tipMasina), culoare(culoare),
                                                                motorizare(motorizare), pret(pret) {}
     virtual ~masina() {}
 
-    Masina() = default;
-
-    ~Masina() = default;
-
-    Masina(const int id) : id(contorId++){};
-
-
-    void setTipMasina(const std::string &ltipMasina) {
-        Tip_masina = ltipMasina;
-    static void setContorId(unsigned int contorId) {
-        contorID = contorId;
-    }
-
-    void setCuloare(const std::string &lculoare) {
-        Masina::culoare = lculoare;
-    void setTipMasina(const std::string &tipMasina) {
+    void setTipMasina(const int &tipMasina) {
         Tip_masina = tipMasina;
     }
 
@@ -42,86 +23,99 @@ public:
         masina::culoare = culoare;
     }
 
-
-    friend std::ostream &operator<<(std::ostream &out, const Masina &masina) {
-        out << "id: " << masina.id << " Tip_masina: " << masina.Tip_masina << " culoare: " << masina.culoare;
-        return out;
-    void setMotorizare(const std::string &motorizare) {
         masina::motorizare = motorizare;
     }
 
-    const std::string &getTipMasina() const {
+    int getTipMasina() const {
         return Tip_masina;
-    void setPret(unsigned int pret) {
-        masina::pret = pret;
     }
 
     const std::string &getCuloare() const {
         return culoare;
     }
 
-    const std::string &getMotorizare() const {
+    float getMotorizare() const {
         return motorizare;
     }
 
-
-    Masina& operator=(const Masina& masina){
+    masina& operator=(const masina& masina){
         this->culoare = culoare;
         this->Tip_masina = Tip_masina;
         this->motorizare = motorizare;
         this->pret = pret;
         return *this;
     }
-
-    Masina &operator++ (){
-        contorId++;
     friend std::ostream &operator<<(std::ostream &out, const masina &masina) {
         out << " Tip_masina: " << masina.Tip_masina << " culoare: " << masina.culoare << "pret: "<<masina.pret;
         return out;
     }
-    void custom_color(const std::string& newcolor){
+
+};
+//"aplicatia" de baza
+class app:public virtual masina{
+private:
+    static int cntMasini;
+    const int nrMasini = 0;
+    static int contor;
+protected:
+public:
+
+    app(const int &tipMasina, const std::string &culoare, const float &motorizare, unsigned int pret,
+        const int nrMasini) : masina(tipMasina, culoare, motorizare, pret), nrMasini(contor++) {}
+
+    virtual ~app() {}
+
+    static std::vector<masina> masini;
+
+    static void afisare_masini(){
+        std::cout<<"Avem disponibile urmatoarele masini: ";
+        for(auto &masina : masini){
+            std::cout << masina;
+        }
+    }
+    static void first_question(){
+        char yn;
+        std::cout<<"Doriti sa cumparati o masina? [Y/N]: ";
+        std::cin>>yn;
+        if (yn == 'Y')
+            std::cout<<"Bine ati venit!";
+        else
+            std::cout << "Ne pare rau vindem doar masini :(";
+
+    }
+
+    friend std::ostream &operator<<(std::ostream &os, const app &app){
+        for(auto & masina : app.masini){
+            os << masina ;
+        }
+        return os;
+    }
+
+};
+
+int app::contor = 0;
+
+class constructor_masina : public virtual masina, public virtual app{
+public:
+
+    void culoareCustom(const std::string& newcolor){
+        culoare = newcolor;
+    }
+    void motorizareCustom(const std::string& newcolor){
         culoare = newcolor;
     }
 };
 
-int Masina::contorId = 0;
-
-class valabilitate{
-class app{
-private:
-    std::vector<Masina> masini_valabile;
-    std::vector<masina> masini;
-    static int contor;
-    const int nrMasini = 0;
-public:
-    valabilitate() = default;
-    virtual ~valabilitate() {}
-    valabilitate(const std::vector<Masina> &lmasinivalabile){
-        this->masini_valabile=lmasinivalabile;
-    app() = default;
-    ~app() = default;
-    app(const std::vector<masina>& obj){
-        this->masini = obj;
-    }
-    friend std::ostream &operator<<(std::ostream &os, const valabilitate &valabil){
-        for (auto &masina : valabil.masini_valabile) {
-    friend std::ostream &operator<<(std::ostream &os, const app &app){
-        for(auto & masina : app.masini){
-            os << masina;
-        }
-        return os;
-    }
-    void add_more(){
-    }
-};
-
-class constructor_masina:public masina{
-public:
-    custom
-};
-
 int main() {
+    static std::vector<masina> masini;
+    masini.emplace_back(1,"alb",1.4,10000);
+    masini.emplace_back(1,"alb",1.4,10000);
+    masini.emplace_back(1,"alb",1.4,10000);
+    masini.emplace_back(1,"alb",1.4,10000);
+    masini.emplace_back(1,"alb",1.4,10000);
 
-    std::cout << "Hello, World!" << std::endl;
+    app::first_question();
+    app::afisare_masini();
+
     return 0;
 }
